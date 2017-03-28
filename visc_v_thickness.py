192c9c442935a85ee1edea7c5373dbbbbd226bbe
Cassie Stuurman <cassiestuurman@gmail.com>
2017-03-28 12:42:27 -0500

    Creating regime model for buckle folds

---
+++ visc_v_thickness.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+plt.axis([1,10,1,10000])
+
+plt.xlabel('Overburden Thickness (m)',fontsize=16,fontweight='bold')
+plt.ylabel('Viscosity Ratio',fontsize=16,fontweight='bold')
+plt.show()
